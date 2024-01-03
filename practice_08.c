b430dd2809cbb13cec01628e0285b71382518262
ARNAV KAUSHIK <83583041+Arnavkshk@users.noreply.github.com>
2024-01-03 16:28:35 +0530

    Update practice_08.c

--- practice_08.c
+++ practice_08.c
@@ -5,12 +5,15 @@ int search(int array[], int n, int x) {
     if (array[i] == x)
       return i;
   return -1;
+
 }
 
 int main() {
   int array[] = {2, 4, 0, 1, 9};
   int x = 1;
   int n = sizeof(array) / sizeof(array[0]);
+ 
   int result = search(array, n, x);
   (result == -1) ? printf("Element not found") : printf("Element found at index: %d", result);
+  return 0;
 }
